625c66844f6591dbb0ce0d6487517099dd869f03
Gigz <gigzpumpking@gmail.com>
2024-12-04 17:36:37 -0800

    Adjusted build settings + fixed quit key

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
             RestartGame();
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             QuitGame();
         }
